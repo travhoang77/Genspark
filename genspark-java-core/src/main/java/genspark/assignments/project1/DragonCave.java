1222ee3a03639c389c387a25fcf530edba5701ee
Travis Hoang <travhoang@gmail.com>
2022-02-09 12:03:51 -0800

    Declarative Style Refactoring

--- genspark-java-core/src/main/java/genspark/assignments/project1/DragonCave.java
+++ genspark-java-core/src/main/java/genspark/assignments/project1/DragonCave.java
@@ -7,9 +7,17 @@ import java.util.Random;
 
 public class DragonCave implements Assignment {
 
+    private static void startGame() {
+        System.out.println("You are in a land full of dragons, In front of you");
+        System.out.println("you see two caves. In one cave the dragon is friendly");
+        System.out.println("and will share his treasure with you. The other dragon");
+        System.out.println("is greedy and hungry and will eat you on sight");
+        System.out.println("Which cave will you go into? (1 or 2)");
+    }
+
     private static void encounterBadDragon() {
         System.out.println("It is dark and spooky...");
-        System.out.println("A large dragon jump out in front of you! He opens his jaw add...");
+        System.out.println("A large dragon jump out in front of you! He opens his jaw and...");
         System.out.println("Gobbles you down in one bite!");
     }
 
@@ -23,14 +31,8 @@ public class DragonCave implements Assignment {
         return new Random().nextInt(2) + 1;
     }
 
-    public static void main(String[] args) {
+    private static int chooseCave() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("You are in a land full of dragons, In front of you");
-        System.out.println("you see two caves. In one cave the dragon is friendly");
-        System.out.println("and will share his treasure with you. The other dragon");
-        System.out.println("is greedy and hungry and will eat you on sight");
-        System.out.println("Which cave will you go into? (1 or 2)");
-
         int userInput = 1;
         do {
             if(userInput != 1 && userInput != 2) {
@@ -39,7 +41,13 @@ public class DragonCave implements Assignment {
             userInput = sc.nextInt();
 
         } while (userInput != 1 && userInput != 2);
+        return userInput;
+    }
 
+    public static void main(String[] args) {
+
+        startGame();
+        int userInput = chooseCave();
         int number = generateOneOrTwo();
 
         System.out.println("You approach the cave...");
